e7501e09596d40f67277544dd0aeda3b729cda01
Victoria Slusareva <70879567+Victoria-Slusareva@users.noreply.github.com>
2020-10-17 22:42:50 +0300

    Add files via upload

---
+++ Lab_7/src/Test_Dequeue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Test_Dequeue
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nВведите 5 карт первого игрока ");
+        String[] cards = scanner.nextLine().split(" ");
+        ArrayDeque<Integer> first_c = new ArrayDeque<>();
+        for (String s : cards) {
+            first_c.add(Integer.parseInt(s));
+        }
+        System.out.println(first_c);
+        ArrayDeque<Integer> second_c = new ArrayDeque<>();
+        System.out.println("Введите 5 карт второго игрока ");
+        cards = scanner.nextLine().split(" ");
+        for (String card : cards) {
+            second_c.add(Integer.parseInt(card));
+        }
+        System.out.println(second_c);
+        System.out.println(Game_Dequeue.Game(first_c, second_c));
+    }
+}
---
+++ Lab_7/src/Test_Stack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test_Stack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nВведите 5 карт первого игрока ");
+        String[] cards = scanner.nextLine().split(" ");
+        Stack<Integer> first_c = new Stack<>();
+        for (int i = cards.length-1; i >=0; i--) {
+            first_c.add(Integer.parseInt(cards[i]));
+        }
+        System.out.println(first_c);
+        Stack<Integer> second_c = new Stack<>();
+        System.out.println("Введите 5 карт второго игрока ");
+        cards = scanner.nextLine().split(" ");
+        for (int i = cards.length-1; i >=0; i--) {
+            second_c.add(Integer.parseInt(cards[i]));
+        }
+        System.out.println(second_c);
+        System.out.println(Game_Stack.Game(first_c, second_c));
+    }
+}
